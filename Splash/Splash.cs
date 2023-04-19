a3663cf85898c1c9f2e0392bab6cab9fa382ab8f
Fireye025 <43316468+Fireye025@users.noreply.github.com>
2023-04-20 00:26:38 +0200

    grab move

--- Splash/Splash.cs
+++ Splash/Splash.cs
@@ -6,7 +6,9 @@ public partial class Splash : FloatingBody
 {
     private bool Impulse = true;
     private const float WaterSpeed = 600.0f;
+    private const float GrabSpeed = 600.0f;
     private PlayerData SplashData = new PlayerData();
+    private Vector2 GrabPoint = new Vector2(0, 0);
     public event OutOfScreenHandler OutOfScreen;
     public override async void _PhysicsProcess(double delta)
 	{
@@ -18,19 +20,31 @@ public partial class Splash : FloatingBody
             GetNode<AnimationPlayer>("AnimationPlayer").Play("move");
             GetNode<GpuParticles2D>("InkParticles").Emitting = true;
         }else{
-            if(IsInWater()){
-                velocity.Y = Mathf.MoveToward(velocity.Y, 0, WaterFriction);
-                velocity.X = Mathf.MoveToward(velocity.X, 0, WaterFriction);
-                if(Impulse && swimming){
-                    velocity += Vector2.FromAngle(Rotation) * WaterSpeed;
-                    GetNode<AnimationPlayer>("AnimationPlayer").Play("move");
+            if(Input.IsActionPressed("grab")){
+                if(GrabPoint != Vector2.Zero){
+                    if((GrabPoint - GlobalPosition).Length() > 10){
+                        Vector2 direction = (GrabPoint - GlobalPosition).Normalized();
+                        velocity = direction * GrabSpeed;
+                    }else{
+                        velocity = Vector2.Zero;
+                    }
                     
-                    Impulse = false;
-                    GetNode<Timer>("Timer").Start();
                 }
             }else{
-                velocity.Y += gravity * (float)delta;
-                velocity.X = Mathf.MoveToward(velocity.X, 0, AirFriction);
+                if(IsInWater()){
+                    velocity.Y = Mathf.MoveToward(velocity.Y, 0, WaterFriction);
+                    velocity.X = Mathf.MoveToward(velocity.X, 0, WaterFriction);
+                    if(Impulse && swimming){
+                        velocity += Vector2.FromAngle(Rotation) * WaterSpeed;
+                        GetNode<AnimationPlayer>("AnimationPlayer").Play("move");
+                        
+                        Impulse = false;
+                        GetNode<Timer>("Timer").Start();
+                    }
+                }else{
+                    velocity.Y += gravity * (float)delta;
+                    velocity.X = Mathf.MoveToward(velocity.X, 0, AirFriction);
+                }
             }
             GetNode<GpuParticles2D>("InkParticles").Emitting = false;
         }
@@ -46,7 +60,8 @@ public partial class Splash : FloatingBody
 		MoveAndSlide();
 
         if(Input.IsActionJustPressed("grab")){
-            GetNode<AnimationPlayer>("AnimationPlayer").Play("grab");
+            GrabPoint = new Vector2(0, 0);
+            GetNode<AnimationPlayer>("ActionAnimationPlayer").Play("grab");
         }
 
         SplashData.ComputeOxygen((float)delta, IsInWater());
@@ -70,15 +85,17 @@ public partial class Splash : FloatingBody
     }
 
     public void _OnGrab(Node2D body){
-        GetNode<AnimationPlayer>("AnimationPlayer").PlayBackwards("grab");
         if(body is Collectible){
             Collectible collectible = (Collectible)body;
             collectible.Grab(this);
+        }else{
+            GrabPoint = GetNode<Area2D>("Tongue").GlobalPosition;
         }
+        GetNode<AnimationPlayer>("ActionAnimationPlayer").PlayBackwards("grab");
     }
 
     public void FinishGrab(){
-        GetNode<AnimationPlayer>("AnimationPlayer").PlayBackwards("grab");
+        GetNode<AnimationPlayer>("ActionAnimationPlayer").PlayBackwards("grab");
     }
 
     public void _OnCollectionAreaBodyEntered(Node2D body){
